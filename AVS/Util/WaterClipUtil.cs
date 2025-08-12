01140e275819340101c57b580e10e3428a23f60d
IronFox <github@portal.delta-works.org>
2025-08-12 18:09:03 +0200

    Add SDF Cutout.  Expose HierarchyAnalyzer

--- AVS/Util/WaterClipUtil.cs
+++ AVS/Util/WaterClipUtil.cs
@@ -1,6 +1,7 @@
 ﻿using AVS.Assets;
 using AVS.Log;
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace AVS.Util
@@ -31,18 +32,22 @@ namespace AVS.Util
                 throw new InvalidOperationException("Seamoth not found. Cannot bind water clip.");
             }
 
+            SDFCutout cutout = seamoth.GetComponentInChildren<SDFCutout>();
+
             WaterClipProxy seamothWCP = seamoth.GetComponentInChildren<WaterClipProxy>();
 
             log.Write($"Binding WaterClipProxy to {target.name} with distance map {distanceMap.name} and bounds {localBounds}");
+
+            //bool existed = target.GetComponent<WaterCausticsGenerator>() != null;
 
             WaterClipProxy waterClip = target.EnsureComponent<WaterClipProxy>();
             waterClip.shape = WaterClipProxy.Shape.DistanceField;
             waterClip.clipMaterial = seamothWCP.clipMaterial;
             waterClip.gameObject.layer = seamothWCP.gameObject.layer;
 
-            var scale = 1.12f;
+            var scale = 1f;// 1.12f;
             log.Write($"Setting up WaterClipProxy with scale {scale}");
-
+            localBounds.center -= target.transform.localPosition;
             localBounds.size *= scale;
             waterClip.distanceFieldMin = localBounds.min;
             waterClip.distanceFieldMax = localBounds.max;
@@ -56,9 +61,9 @@ namespace AVS.Util
             float originalScale = seamothWCP.transform.lossyScale.x;
 
             float foam = waterClip.waterSurface.SafeGet(x => x.foamDistance, 5f);
-            //borderSizeScaled.x = foam / target.transform.lossyScale.x;
-            //borderSizeScaled.y = foam / target.transform.lossyScale.y;
-            //borderSizeScaled.z = foam / target.transform.lossyScale.z;
+            borderSizeScaled.x = foam / target.transform.lossyScale.x;
+            borderSizeScaled.y = foam / target.transform.lossyScale.y;
+            borderSizeScaled.z = foam / target.transform.lossyScale.z;
 
             log.Write($"Border size: {borderSizeScaled} from foam {foam}, Seamoth lossy scale was {originalScale}");
 
@@ -73,8 +78,48 @@ namespace AVS.Util
             waterClip.clipMaterial = meshRenderer.material;
             waterClip.UpdateMaterial();
 
+            if (cutout != null)
+            {
+                log.Write("Setting up SDF cutout");
+                var existed = target.GetComponent<SDFCutout>();
+                var nCutout = existed.OrRequired(() => target.AddComponent<SDFCutout>());
+                nCutout.distanceFieldMin = localBounds.min;
+                nCutout.distanceFieldMax = localBounds.max;
+                nCutout.distanceFieldBounds = localBounds;
+                nCutout.distanceFieldTexture = distanceMap;
+                nCutout.distanceFieldSizeRcp = new Vector3(
+                    1f / localBounds.size.x,
+                    1f / localBounds.size.y,
+                    1f / localBounds.size.z);
+                if (existed == null)
+                    UWE.CoroutineHost.StartCoroutine(LateReconfigure(log, nCutout, localBounds, distanceMap));
+            }
+            else
+                log.Error("SDFCutout not found on Seamoth. Cannot set up");
+
             log.Write($"All set. Water clip proxy bound");
 
+        }
+
+        private static IEnumerator LateReconfigure(LogWriter log, SDFCutout nCutout, Bounds localBounds, Texture3D distanceMap)
+        {
+            for (int i = 0; i < 20; i++)
+                yield return new WaitForEndOfFrame();
+            if (nCutout == null)
+            {
+                log.Write("Cannot ref-fix nCutout: gone");
+                yield break;
+            }
+            log.Write("Re-fixing nCutout");
+            nCutout.distanceFieldMin = localBounds.min;
+            nCutout.distanceFieldMax = localBounds.max;
+            nCutout.distanceFieldBounds = localBounds;
+            nCutout.distanceFieldTexture = distanceMap;
+            nCutout.distanceFieldSizeRcp = new Vector3(
+                1f / localBounds.size.x,
+                1f / localBounds.size.y,
+                1f / localBounds.size.z);
+            log.Write("Done");
         }
 
         private static void DestroyComponent<T>(GameObject target, LogWriter log) where T : Component
@@ -99,17 +144,17 @@ namespace AVS.Util
         /// <param name="target">Target object that may contain the WaterClipProxy and its renderers</param>
         public static void UnbindProxy(LogWriter log, GameObject target)
         {
-            log = log.Tag("WCP");
-            if (target == null)
-            {
-                log.Error("Target is null. Cannot unbind water clip proxy.");
-                return;
-            }
-            DestroyComponent<WaterClipProxy>(target, log);
-            DestroyComponent<MeshRenderer>(target, log);
-            DestroyComponent<MeshFilter>(target, log);
+            //log = log.Tag("WCP");
+            //if (target == null)
+            //{
+            //    log.Error("Target is null. Cannot unbind water clip proxy.");
+            //    return;
+            //}
+            //DestroyComponent<WaterClipProxy>(target, log);
+            //DestroyComponent<MeshRenderer>(target, log);
+            //DestroyComponent<MeshFilter>(target, log);
 
-            log.Write($"Unbinding complete. Water clip proxy unbound from {target.name}.");
+            //log.Write($"Unbinding complete. Water clip proxy unbound from {target.name}.");
         }
     }
 }
