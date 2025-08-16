0cbbd1e85763b574c16a50380c7a3fdabd862a4a
IronFox <github@portal.delta-works.org>
2025-08-16 18:53:19 +0200

    Add missing documentation

--- AVS/Patches/SDFCutoutPatcher.cs
+++ AVS/Patches/SDFCutoutPatcher.cs
@@ -3,6 +3,11 @@ using HarmonyLib;
 
 namespace AVS.Patches;
 
+/// <summary>
+/// A patching class for the SDFCutout component, providing functionality
+/// to suppress the execution of the Start method when a cutout instance has been initialized externally.
+/// Uses Harmony to intercept the Start method of SDFCutout.
+/// </summary>
 [HarmonyPatch(typeof(SDFCutout))]
 public class SDFCutoutPatcher
 {
@@ -20,6 +25,16 @@ public class SDFCutoutPatcher
         suppressed.Add(cutout);
     }
 
+    /// <summary>
+    /// A Harmony prefix patch for the Start method of the SDFCutout class.
+    /// Controls whether the Start method should execute, based on whether the cutout instance
+    /// has been marked to suppress its Start execution.
+    /// </summary>
+    /// <param name="__instance">The instance of the SDFCutout whose Start method is being intercepted.</param>
+    /// <returns>
+    /// Returns false if the Start method should be skipped because the instance is suppressed.
+    /// Returns true if the Start method should execute normally.
+    /// </returns>
     [HarmonyPrefix]
     [HarmonyPatch(nameof(SDFCutout.Start))]
     public static bool StartPatch(SDFCutout __instance)
