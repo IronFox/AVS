c3c55a610c05404b23b268bf12acb800ab0e3360
IronFox <github@portal.delta-works.org>
2025-09-08 13:46:32 +0200

    Try-catch Vehicle.collisionModule assignment (XBox issues)

--- AVS/VehicleBuilding/AvsVehicleBuilder.cs
+++ AVS/VehicleBuilding/AvsVehicleBuilder.cs
@@ -636,7 +636,14 @@ internal static class AvsVehicleBuilder
         SetupDamageComponents(av, seamoth);
         SetupLavaLarvaAttachPoints(av);
         SetupDenyBuildingTags(av);
-        av.collisionModel = av.Com.CollisionModel;
+        try
+        {
+            av.collisionModel = av.Com.CollisionModel;
+        }
+        catch (MissingFieldException ex)
+        {
+            log.Error("Assignment to Vehicle.collisionModel failed. Subnautica will not be able to manage the collision model", ex);
+        }
 
         if (av is Submarine sub3)
         {
