343bb988cebea535dac492e2f0ae7333e3177457
IronFox <github@portal.delta-works.org>
2025-10-20 17:02:51 +0200

    Expose game sound volumes

---
+++ AVS/Audio/Volume.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVS.Audio
+{
+    /// <summary>
+    /// Game sound volume access helper. Necessary only when creating sounds outside the control of AVS
+    /// </summary>
+    public static class Volume
+    {
+        /// <summary>
+        /// Gets the current master volume level for the sound system.
+        /// </summary>
+        public static float Master => SoundSystem.GetMasterVolume();
+
+        /// <summary>
+        /// Gets the current music volume level.
+        /// </summary>
+        public static float Music => SoundSystem.GetMusicVolume();
+
+        /// <summary>
+        /// Gets the current global voice volume level.
+        /// </summary>
+        public static float Voice => SoundSystem.GetVoiceVolume();
+
+        /// <summary>
+        /// Gets the current ambient sound volume level.
+        /// </summary>
+        public static float Ambient => SoundSystem.GetAmbientVolume();
+    }
+}
