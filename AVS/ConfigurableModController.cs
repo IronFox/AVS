ee1243c95e0be5553237caf9aabd1acef18c1f9d
IronFox <github@portal.delta-works.org>
2025-10-20 13:25:21 +0200

    Minor improvement. Update assembly

--- AVS/ConfigurableModController.cs
+++ AVS/ConfigurableModController.cs
@@ -75,7 +75,7 @@ namespace AVS
                 try
                 {
 
-                    if (EnumHandler.TryAddEntry<GameInput.Button>($"AVS_{ModName}_Button_{f.Name}", out var builder))
+                    if (EnumHandler.TryAddEntry<GameInput.Button>($"AVS.{ModName}.Button.{f.Name}", out var builder))
                     {
                         bool anyTranslation = false;
                         if (!b.LabelLocalizationKey.IsNullOrEmpty() && languages.Count > 0)
